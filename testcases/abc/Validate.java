29c2503476223e57834b159747e25eb6381b06db
Dao Duy Khang <duykhangkma@gmail.com>
2020-11-24 21:35:47 +0700

    Init project

---
+++ testcases/abc/Validate.java
@@ -0,0 +1,19 @@
+package abc;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Validate {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
